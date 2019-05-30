9431b0ce86785185925c245af01c24a348cef9bb
Pedro <pedrocolrj@gmailc.om>
2019-05-30 02:23:01 -0300

    dfg

--- julia101.jl
+++ julia101.jl
@@ -1,7 +1,7 @@
 ### matriz
 
-A = [[1 0];[0 1]]
 
+A = [[1 0];[0 1]]
 abs.(A)
 
 for i = 1:100
@@ -12,12 +12,12 @@ f(x) = x^2
 
 # ou
 
-function quadrado
-    x^2
+function quadrado(x)
+    x = x^2
+    return x
 end
 
 quadrado(2)
-
 
 using Plots
 
@@ -32,12 +32,23 @@ z = cumsum(y)
 plot(z)
 
 w = 0.5*x + randn(500)
+d = zeros(500)
+d[1] = 1
+
+
+
 w = w + cumsum(randn(500))
+vetor = ones(500)
+vetor = cumsum(vetor)
+
+
+
+y = sin(vetor) + randn(500)
 plot(w)
+
 
 plot!(x)
 savefig("figura1.png")
-
 
 using Optim
 
